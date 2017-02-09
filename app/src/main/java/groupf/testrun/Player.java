1e13dc0d1fe6c35c40be1c1a1d7ae3fc643535ac
Jose Matundan <matundjf@myumanitoba.ca>
2017-02-08 20:02:47 -0600

    Testruns

---
+++ app/src/main/java/groupf/testrun/Player.java
@@ -0,0 +1,23 @@
+package groupf.testrun;
+
+public class Player {
+    private String name;
+    private int img;
+    public Player(String name,int img) {
+        // TODO Auto-generated constructor stub
+        this.name=name;
+        this.img=img;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getImg() {
+        return img;
+    }
+    public void setImg(int img) {
+        this.img = img;
+    }
+}
